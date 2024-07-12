fb46e05588fea233a7bcf70a1cd5a4edf8b1cbd8
Nanai Jua <me@n7i.dev>
2024-07-13 04:48:43 +0900

    feat: rewrite as concurrent

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,45 @@
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use anyhow::Context as _;
 use anyhow::anyhow;
+use anyhow::Context as _;
 
-use serenity::prelude::Context;
-use serenity::model::prelude::{Ready, ChannelId, VoiceState};
 use serenity::builder::CreateMessage;
+use serenity::model::prelude::{ChannelId, Ready, VoiceState};
+use serenity::prelude::Context;
 
 use tracing::{debug, error, info, instrument};
 
 #[shuttle_runtime::main]
-async fn serenity(#[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore) -> shuttle_serenity::ShuttleSerenity {
-    let token = secrets.get("DISCORD_TOKEN").ok_or(anyhow!("`DISCORD_TOKEN` is not provided!"))?;
+async fn serenity(
+    #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    let token = secrets
+        .get("DISCORD_TOKEN")
+        .ok_or(anyhow!("`DISCORD_TOKEN` is not provided!"))?;
 
-    let notify_channel_id = secrets.get("NOTIFY_CHANNEL_ID").ok_or(anyhow!("`NOTIFY_CHANNEL_ID` is not provided!"))?;
-    let notify_channel_id = ChannelId::new(notify_channel_id.parse().context("`NOTIFY_CHANNEL_ID` is not able to parse!")?);
+    let notify_channel_id = ChannelId::new(
+        secrets
+            .get("NOTIFY_CHANNEL_ID")
+            .ok_or(anyhow!("`NOTIFY_CHANNEL_ID` is not provided!"))?
+            .parse()
+            .context("`NOTIFY_CHANNEL_ID` is not able to parse!")?,
+    );
 
-    let logging_channel_id = secrets.get("LOGGING_CHANNEL_ID").ok_or(anyhow!("`LOGGING_CHANNEL_ID` is not provided!"))?;
-    let logging_channel_id = ChannelId::new(logging_channel_id.parse().context("`LOGGING_CHANNEL_ID` is not able to parse!")?);
+    let logging_channel_id = ChannelId::new(
+        secrets
+            .get("LOGGING_CHANNEL_ID")
+            .ok_or(anyhow!("`LOGGING_CHANNEL_ID` is not provided!"))?
+            .parse()
+            .context("`LOGGING_CHANNEL_ID` is not able to parse!")?,
+    );
 
-    let handler = Handler { notify_channel_id, logging_channel_id };
+    let handler = Handler {
+        notify_channel_id,
+        logging_channel_id,
+    };
 
-    let intents = serenity::prelude::GatewayIntents::GUILDS | serenity::prelude::GatewayIntents::GUILD_VOICE_STATES;
+    let intents = serenity::prelude::GatewayIntents::GUILDS
+        | serenity::prelude::GatewayIntents::GUILD_VOICE_STATES;
 
     let client = serenity::Client::builder(&token, intents)
         .event_handler(handler)
@@ -53,28 +71,33 @@ impl serenity::client::EventHandler for Handler {
     async fn voice_state_update(&self, ctx: Context, old: Option<VoiceState>, new: VoiceState) {
         debug!(kind = "voice_state_update", "received gateway event");
 
-        let timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
+        tokio::join! {
+            self.handle_as_record(&ctx, &old, &new),
+            self.handle_as_notification(&ctx, &old, &new),
+        };
+    }
+}
 
-        let VoiceState { guild_id, channel_id, user_id, session_id, member, .. } = new;
-
-        let action_verb = if channel_id.is_some() { "join" } else { "leave" };
-        let action_prep = if channel_id.is_some() { "into" } else { "from" };
-
-        let ensured_channel_id = channel_id.unwrap_or_else(|| old.unwrap().channel_id.unwrap());
-
-        let guild_id = guild_id.map(|v| v.to_string()).unwrap_or("{empty}".to_owned());
-        let channel_id = channel_id.map(|v| v.to_string()).unwrap_or("{empty}".to_owned());
-        let member = member.unwrap();
-
-        let username = member.user.name;
-        let name = if let Some(nick) = member.nick {
-            format!("{nick} ({username})")
-        } else {
-            username
+impl Handler {
+    #[instrument(skip_all, name = "Handler::handle_as_record")]
+    async fn handle_as_record(&self, ctx: &Context, _: &Option<VoiceState>, new: &VoiceState) {
+        let VoiceState {
+            guild_id: Some(guild_id),
+            channel_id,
+            user_id,
+            session_id,
+            ..
+        } = &new
+        else {
+            return error!("guild_id is not present!");
         };
 
-        {
-            let content = format!("
+        let channel_id = channel_id
+            .map(|v| v.to_string())
+            .unwrap_or("{empty}".to_owned());
+
+        let content = format!(
+            "
                 ```
                 v: 0
                 g: {guild_id}
@@ -82,30 +105,77 @@ impl serenity::client::EventHandler for Handler {
                 s: {session_id}
                 c: {channel_id}
                 ```
-            ").replace("\n                ", "").trim().to_owned();
+            ",
+        )
+        .replace("\n                ", "\n")
+        .trim()
+        .to_owned();
 
-            let result = self.logging_channel_id.send_message(&ctx, CreateMessage::new().content(content)).await;
-            match result {
-                Ok(msg) => {
-                    debug!(kind = "log", message_id = ?msg.id, "successfully sent");
-                },
-                Err(e) => {
-                    error!(kind = "log", error = ?e, "error occurred while sending message");
-                },
+        match self
+            .logging_channel_id
+            .send_message(&ctx, CreateMessage::new().content(content))
+            .await
+        {
+            Ok(msg) => {
+                debug!(message_id = ?msg.id, "successfully sent");
+            }
+            Err(e) => {
+                error!(error = ?e, "error occurred while sending message");
             }
         }
+    }
 
+    #[instrument(skip_all, name = "Handler::handle_as_notification")]
+    async fn handle_as_notification(
+        &self,
+        ctx: &Context,
+        old: &Option<VoiceState>,
+        new: &VoiceState,
+    ) {
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let VoiceState {
+            channel_id,
+            member: Some(member),
+            ..
+        } = &new
+        else {
+            return error!("member is not present!");
+        };
+
+        let Some(ensured_channel_id) =
+            channel_id.or_else(|| old.clone().map(|vs| vs.channel_id).flatten())
+        else {
+            return error!("cannot ensure channel_id!");
+        };
+
+        let name = if let Some(nick) = &member.nick {
+            &format!("{nick} ({username})", username = &member.user.name)
+        } else {
+            &member.user.name
+        };
+
+        let (verb, prep) = if channel_id.is_some() {
+            ("join", "into")
+        } else {
+            ("leave", "from")
+        };
+
+        let content = format!("{name} {verb} {prep} <#{ensured_channel_id}> at <t:{timestamp}:R>");
+
+        match self
+            .notify_channel_id
+            .send_message(&ctx, CreateMessage::new().content(content))
+            .await
         {
-            let content = format!("{name} {action_verb} {action_prep} <#{ensured_channel_id}> at <t:{timestamp}:R>");
-
-            let result = self.notify_channel_id.send_message(&ctx, CreateMessage::new().content(content)).await;
-            match result {
-                Ok(msg) => {
-                    debug!(kind = "notify", message_id = ?msg.id, "successfully sent");
-                },
-                Err(e) => {
-                    error!(kind = "notify", error = ?e, "error occurred while sending message");
-                },
+            Ok(msg) => {
+                debug!(message_id = ?msg.id, "successfully sent");
+            }
+            Err(e) => {
+                error!(error = ?e, "error occurred while sending message");
             }
         }
     }
